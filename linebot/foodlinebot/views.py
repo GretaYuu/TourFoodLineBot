9166ac74093304c2beebad6584b5df78ba98f5ee
GretaYuu <132889038+GretaYuu@users.noreply.github.com>
2023-05-28 14:45:45 +0800

    Add files via upload

--- linebot/foodlinebot/views.py
+++ linebot/foodlinebot/views.py
@@ -2,28 +2,33 @@ from django.shortcuts import render
 from django.http import HttpResponse, HttpResponseBadRequest, HttpResponseForbidden
 from django.views.decorators.csrf import csrf_exempt
 from django.conf import settings
+
 from linebot import LineBotApi, WebhookParser
 from linebot.exceptions import InvalidSignatureError, LineBotApiError
 from linebot.models import MessageEvent, TextSendMessage
 
-#line_bot_api = LineBotApi(settings.LINE_CHANNEL_ACCESS_TOKEN)
-line_bot_api = LineBotApi('2YnfFy6Rf+qkkuWedKQHRfJ21Qh/Ztvp6Mx3BXz26tcEwvIQ4QAxrIoHS5Shno37Gpr0d021ZiYMVP7z2FMdRgzE+VKAluhCPA1mchUnBLnAnjAkzS9kkFxGI02rjQECninRwLJ74nJN+lbuAM9+7wdB04t89/1O/w1cDnyilFU=')
+from .scraper import IFoodie
+
+# line_bot_api = LineBotApi(settings.LINE_CHANNEL_ACCESS_TOKEN)
+line_bot_api = LineBotApi(
+    '2YnfFy6Rf+qkkuWedKQHRfJ21Qh/Ztvp6Mx3BXz26tcEwvIQ4QAxrIoHS5Shno37Gpr0d021ZiYMVP7z2FMdRgzE+VKAluhCPA1mchUnBLnAnjAkzS9kkFxGI02rjQECninRwLJ74nJN+lbuAM9+7wdB04t89/1O/w1cDnyilFU=')
 parser = WebhookParser('fd992488416e202095dfa8d28d053992')
+
 
 @csrf_exempt
 def callback(request):
- 
+
     if request.method == 'POST':
         signature = request.META['HTTP_X_LINE_SIGNATURE']
         body = request.body.decode('utf-8')
- 
+
         try:
             events = parser.parse(body, signature)  # 傳入的事件
         except InvalidSignatureError:
             return HttpResponseForbidden()
         except LineBotApiError:
             return HttpResponseBadRequest()
- 
+
         for event in events:
             if isinstance(event, MessageEvent):  # 如果有訊息事件
                 line_bot_api.reply_message(  # 回復傳入的訊息文字
--- linebot/mylinebot/urls.py
+++ linebot/mylinebot/urls.py
@@ -1,29 +1,13 @@
-"""
-URL configuration for mylinebot project.
-
-The `urlpatterns` list routes URLs to views. For more information please see:
-    https://docs.djangoproject.com/en/4.2/topics/http/urls/
-Examples:
-Function views
-    1. Add an import:  from my_app import views
-    2. Add a URL to urlpatterns:  path('', views.home, name='home')
-Class-based views
-    1. Add an import:  from other_app.views import Home
-    2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
-Including another URLconf
-    1. Import the include() function: from django.urls import include, path
-    2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
-"""
-#而為了要將這個Django應用程式(APP)的網址也加入到專案主程式中，所以，在Django專案主程式下的urls.py檔案中，加入以下的網址設定
+from django.urls import path, include
 from django.contrib import admin
-from django.urls import path
-'''
 import os
 from django.core.wsgi import get_wsgi_application
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'mylinebot.settings')
 application = get_wsgi_application()
-'''
+
+
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('foodlinebot/', include('foodlinebot.urls'))
 ]
